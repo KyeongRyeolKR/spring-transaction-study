389458b961657244bd95d6e600376f7b55a0d148
김경렬 <rlarudfuf802@naver.com>
2023-09-09 23:57:17 +0900

    트랜잭션 전파 활용1 - 예제 프로젝트 시작

---
+++ src/main/java/hello/springtx/propagation/MemberRepository.java
@@ -0,0 +1,32 @@
+package hello.springtx.propagation;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    @Transactional
+    public void save(Member member) {
+        log.info("member 저장");
+        em.persist(member);
+    }
+
+    /**
+     * PK로 조회하지 않고 username으로 조회하기 때문에 JPQL을 직접 사용해야함
+     */
+    public Optional<Member> find(String username) {
+        return em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", username)
+                .getResultList().stream().findAny();
+    }
+}
